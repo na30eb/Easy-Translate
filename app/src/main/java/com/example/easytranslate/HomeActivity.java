185c370c8b75b202db8123e700c9b8e0fd351baa
na30eb <r.sulfus2@gmail.com>
2021-12-31 02:49:57 +0330

    Home activity commit

--- app/src/main/java/com/example/easytranslate/HomeActivity.java
+++ app/src/main/java/com/example/easytranslate/HomeActivity.java
@@ -3,40 +3,54 @@ package com.example.easytranslate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+
 import com.google.android.material.navigation.NavigationBarView;
 
 import org.json.JSONObject;
 
+import java.util.List;
 import java.util.Queue;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class HomeActivity extends AppCompatActivity {
-    Spinner flang,slang ;
-    Button btntranslate ;
+    Spinner lang1,lang2;
+    EditText input;
+    Button btn;
+    apiInterface request;
+    TextView output;
+    String url= "http://api.vajehyab.com/v3/";
+    String  q = input.toString();
+    String token = "68396.RFjz899q8QVoxLRxykPsEzk2yJccOwXjtiwVAUx4";
+    String type = "exact";
+    String filter="dehkhoda";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        flang=findViewById(R.id.spinner);
-        slang=findViewById(R.id.spinner2);
-        btntranslate=findViewById(R.id.btntrans);
+        lang1=findViewById(R.id.spinner);
+        lang2=findViewById(R.id.spinner2);
+        input=findViewById(R.id.editTextTextMultiLine);
+        output=findViewById(R.id.textView);
+        btn=findViewById(R.id.btntrans);
 
 
-        flang.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+        lang1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                // Toast.makeText(HomeActivity.this,parent.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
@@ -48,33 +62,25 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        btntranslate.setOnClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                // Instantiate the RequestQueue.
-                RequestQueue queue = Volley.newRequestQueue(HomeActivity.this);
-                String url ="http://api.vajehyab.com/v3/word?token=68396.RFjz899q8QVoxLRxykPsEzk2yJccOwXjtiwVAUx4&title=سلام&db=en2fa&num=1";
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                request = apiclient.getapiclient(url).create(apiInterface.class);
+                request.getdata(token,q,type,filter).enqueue(new Callback<List<modelClass>>() {
+                    @Override
+                    public void onResponse(Call<List<modelClass>> call, Response<List<modelClass>> response) {
+                        Toast.makeText(HomeActivity.this, "everthong is ok !"+ response.toString(), Toast.LENGTH_SHORT).show();
 
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                Toast.makeText(HomeActivity.this,response.toString() , Toast.LENGTH_SHORT).show();
-                            }
-                        }, new Response.ErrorListener() {
+                    }
 
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                // TODO: Handle error
-                                Toast.makeText(HomeActivity.this,"something wrong" , Toast.LENGTH_SHORT).show();
+                    @Override
+                    public void onFailure(Call<List<modelClass>> call, Throwable t) {
+                        Toast.makeText(HomeActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.e("Error",t.getMessage()+"");
+                    }
+                });
 
-                            }
-                        });
-
-
-
-                // Toast.makeText(HomeActivity.this,"Translating "+flang.getSelectedItem().toString()+" to "+slang.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
